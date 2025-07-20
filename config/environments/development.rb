bd98f0a2010bdb83ef2cdc605bcc98dcd8355498
enogrob <enogrob@gmail.com>
2025-07-20 17:47:33 -0300

    setup-ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  # Allow all ngrok domains in development
+  config.hosts << /[a-z0-9-]+\.ngrok(-free)?\.app/
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Make code changes take effect immediately without server restart.
